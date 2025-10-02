c79d8fa2ab0586eb90ae23450dc178378bb5be09
banzaiaiai <drago2004@outlook.fr>
2025-10-02 10:52:31 +0200

    tous va bien avant teste

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # HeroRealms
+
+# Interface Graphique
+SFML parait une bonne idée 
+preuve : https://www.youtube.com/watch?v=FrpcG1bfcx4
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,18 @@
+
 #include <iostream>
-int main (){
-  std::cout << "feur";
-  return 0;
+using namespace std;
+
+// Une fonction classique
+int add(int a, int b) {
+    return a + b;
 }
+
+// Une fonction qui prend un pointeur de fonction en paramètre
+int compute(int x, int y, int (*func)(int, int)) {
+    return func(x, y);
+}
+
+int main() {
+    cout << compute(5, 3, add) << endl; // appelle add(5,3)
+}
+
