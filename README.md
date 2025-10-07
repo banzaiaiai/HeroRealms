14a4223732d24be8834298467c5689571f7c2350
banzaiaiai <drago2004@outlook.fr>
2025-10-07 15:58:22 +0200

    ajout bibliotheque graphique

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # HeroRealms
 
 # Interface Graphique
+install SFML 
 SFML parait une bonne idée 
 preuve : https://www.youtube.com/watch?v=FrpcG1bfcx4
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,24 @@
+#include <SFML/Graphics.hpp>
 
-#include <iostream>
-using namespace std;
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
 
-// Une fonction classique
-int add(int a, int b) {
-    return a + b;
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
 }
-
-// Une fonction qui prend un pointeur de fonction en paramètre
-int compute(int x, int y, int (*func)(int, int)) {
-    return func(x, y);
-}
-
-int main() {
-    cout << compute(5, 3, add) << endl; // appelle add(5,3)
-}
-
