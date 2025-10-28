b5a5da277c8fcc1c9d6d9bc63d0afdbcdd5e7a79
banzaiaiai <drago2004@outlook.fr>
2025-10-28 14:24:52 +0100

    pointeur régler
    Tous est régler sur les pointeur
    Il faut juste faire la vérification des achat régle métier

--- src/frontEnd/SFMLGame.cpp
+++ src/frontEnd/SFMLGame.cpp
@@ -134,7 +134,7 @@ void SFMLGame::handleMouseClick(int mouseX, int mouseY) {
     if (_carteSelectionnee) {
         _carteSelectionnee->setSelected(false);
         // Je pense pas utile
-        // _carteSelectionnee = nullptr;
+        _carteSelectionnee = nullptr;
         _carteSelectionneeId = -1;
     }
     
@@ -164,7 +164,8 @@ void SFMLGame::handleMouseClick(int mouseX, int mouseY) {
 }
 
 void SFMLGame::handleMouseRelease(int mouseX, int mouseY) {
-    if (!_carteSelectionnee || !_zoneSource || _carteSelectionneeId == -1) return;
+    if (!_carteSelectionnee || !_zoneSource || _carteSelectionneeId == -1) 
+        return;
     
     sf::Vector2f mousePos = _window.mapPixelToCoords(sf::Vector2i(mouseX, mouseY));
     ZoneCarte* zoneCible = _zones.getZoneContenant(mousePos);
@@ -185,7 +186,7 @@ void SFMLGame::handleMouseRelease(int mouseX, int mouseY) {
     
     _carteSelectionnee->setSelected(false);
     // Je pense pas utile
-    // _carteSelectionnee = nullptr;
+    _carteSelectionnee = nullptr;
     _carteSelectionneeId = -1;
     _zoneSource = nullptr;
 }
@@ -250,9 +251,16 @@ void SFMLGame::render() {
     _window.display();
 }
 
+
+// Erreur ici 
+/**
+    To DO :
+    Corriger le deplacementValide pour qu'il prenne en compte les règles du jeu.
+**/
 bool SFMLGame::deplacementValide(ZoneCarte* source, ZoneCarte* cible, int carteId) {
-    if (!source || !cible || carteId == -1 || !_partie) return false;
-    
+    if (!source || !cible || carteId == -1 || !_partie) 
+        return false;
+    /*
     std::string sourceNom = source->getNom();
     std::string cibleNom = cible->getNom();
     
@@ -297,6 +305,8 @@ bool SFMLGame::deplacementValide(ZoneCarte* source, ZoneCarte* cible, int carteI
     }
     
     return false;
+    */
+    return true;
 }
 
 void SFMLGame::appliquerDeplacementLogique(int carteId, ZoneCarte* source, ZoneCarte* cible) {
@@ -326,7 +336,7 @@ void SFMLGame::appliquerDeplacementLogique(int carteId, ZoneCarte* source, ZoneC
         // Récupérer la carte depuis la rivière (Partie)
         auto carte = _partie->retirerCarteRiviere(carteId);
         if (carte) {
-            joueur->ajouterCarte(std::move(carte), ZoneType::Main);
+            joueur->ajouterCarte(std::move(carte), ZoneType::Defausse);
             std::cout << "Carte achetée depuis le marché" << std::endl;
         }
     }
