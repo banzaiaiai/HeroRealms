2391aa562f7dbba796ccbad3af41500b59707aea
banzaiaiai <drago2004@outlook.fr>
2025-11-02 13:37:36 +0100

    stun champ

--- include/backEnd/Carte/Champion.hpp
+++ include/backEnd/Carte/Champion.hpp
@@ -9,6 +9,7 @@ private:
     int _pvTotal;
     bool _gardien;
     bool _activer;
+    bool _estStun=false;
 
 public:
     Champion(const std::string& name, int coupOr, Faction faction,
@@ -18,11 +19,13 @@ public:
     inline int getPvTotal() const { return _pvTotal; }
     inline bool getGardien() const { return _gardien; }
     inline bool getActiver() const { return _activer; }
+    inline bool getEstStun() const { return _estStun; }
 
     // Setters
     inline void setPvTotal(int pvTotal) { _pvTotal = pvTotal; }
     inline void setGardien(bool gardien) { _gardien = gardien; }
     inline void setActiver(bool activer) { _activer = activer; }
+    inline void setEstStun(bool estStun) { _estStun = estStun; }
 
     bool estChampion() const override { return true; }
     // Apply damage to the champion
--- src/backEnd/Effect/StunCard.cpp
+++ src/backEnd/Effect/StunCard.cpp
@@ -1,10 +1,36 @@
 #include "backEnd/Effect/StunCard.hpp"
+#include "backEnd/Carte/Champion.hpp"
 #include "backEnd/Joueur.hpp"
-
+#include "frontEnd/Overlay.hpp"
+#include "backEnd/Partie.hpp"
 
 StunCard::StunCard(int duration) : _duration(duration) 
 {
 
 }
 StunCard::~StunCard() {}
-void StunCard::applyEffect(Joueur *joeur){}
+void StunCard::applyEffect(Joueur *joeur){
+    Overlay overlay;
+    while (true) {
+        Partie *partie = joeur->getPartie();
+        Joueur* autreJoueur = partie->getAutreJoueurActuelle();
+        int carteId = overlay.openOverlay("SelectChampion", autreJoueur->getPlateau());
+        if (carteId == -1) {
+            std::cerr << "Aucune carte sélectionnée pour le stun." << std::endl;
+            continue; // Sortir si aucune carte n'est sélectionnée 
+        }
+        Carte* carteJouee = joeur->getCarteById(carteId);
+        if (!carteJouee) {
+            std::cerr << "Probléme de carte dans le stun: id introuver"<<std::endl;
+            continue;
+        }
+        Champion* ch = dynamic_cast<Champion*>(carteJouee);
+        if (!ch) {
+            std::cerr << "La carte ciblée n'est pas un Champion" << std::endl;
+            continue;
+        }
+        ch->setActiver(true);
+        std::cout << "Champion stun avec succès" << std::endl;
+        break;
+    }
+}
