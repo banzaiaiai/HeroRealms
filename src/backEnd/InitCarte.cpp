d6413a40d1b8a83fddd3cf145e811254a3eb5939
banzaiaiai <drago2004@outlook.fr>
2025-11-03 22:28:05 +0100

    fait

--- src/backEnd/InitCarte.cpp
+++ src/backEnd/InitCarte.cpp
@@ -262,7 +262,7 @@ std::vector<std::unique_ptr<Carte>> InitCarte::marcher() {
                     });
                     
     addNonPermanent(res, 1, "Deception", 5, Faction::Guilde, 
-                    "assets/carte/BAS-EN-26-deception.jpg",
+                    "assets/carte/BAS-EN-026-deception.jpg",
                     NonPermanent::Type::Action,
                     {
                         {EventType::OnPlay,{std::make_shared<GainGold>(2),
